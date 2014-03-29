8d4d26dc2481a7e384dac189a543ef31014c4d41
samujjwal <samujjwal@outlook.com>
2014-03-29 11:48:57 -0500

    Task.py with functionalities (get most popular and get least popular term)

--- ml/mywork/task1.py
+++ ml/mywork/task1.py
@@ -10,3 +10,36 @@ we want to glean the following insights:
 3. How many types of guitars are mentioned across all the deals?
 
 """
+import string
+from collections import Counter
+
+#import nltk
+from nltk.corpus import stopwords
+
+#nltk.download("stopwords")
+counter=Counter()
+def explore_corpus(corpus_fname):
+    """ This function reads the text file 'corpus_fname', tokenizes the text file, and
+    creates a list of words->count to produce the most popular and the least popular terms 
+    
+    corpus_fname - data file name to be explored 
+    
+    """
+    raw_corpus=open(corpus_fname)
+    sentence_list=[[word for word in line.lower().translate(None,string.punctuation).split() 
+                    if word not in stopwords.words("english")] for line in raw_corpus] 
+
+    for vec in sentence_list:
+        for word in vec:
+            counter[word] += 1
+    print most_popular_term(1)
+    print least_popular_term(1)
+    #print guitar_types_count(sentence_list)
+    
+def most_popular_term(n):
+    return counter.most_common(n)
+
+def least_popular_term(n):
+    return counter.most_common()[-n:]
+    
+explore_corpus('../data/deals.txt')
