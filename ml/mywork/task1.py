f19ecdece6cd0f496564d6b275bb1c2a23108ca2
samujjwal <samujjwal@outlook.com>
2014-03-29 16:59:32 -0500

    Task1.py modified to include function returning the number of guitar types

--- ml/mywork/task1.py
+++ ml/mywork/task1.py
@@ -13,11 +13,13 @@ we want to glean the following insights:
 import string
 from collections import Counter
 
-#import nltk
+import nltk
 from nltk.corpus import stopwords
 
-#nltk.download("stopwords")
+#uncomment the line below if the system doesnot have nltk data downloaded (only for the first execution)
+#nltk.download()
 counter=Counter()
+
 def explore_corpus(corpus_fname):
     """ This function reads the text file 'corpus_fname', tokenizes the text file, and
     creates a list of words->count to produce the most popular and the least popular terms 
@@ -26,20 +28,59 @@ def explore_corpus(corpus_fname):
     
     """
     raw_corpus=open(corpus_fname)
-    sentence_list=[[word for word in line.lower().translate(None,string.punctuation).split() 
-                    if word not in stopwords.words("english")] for line in raw_corpus] 
+    data_list=[]
+    #create a tokenized list of deals data, where all punctuations and english stopwords are removed
+    data_list=[[word for word in line.lower().translate(None,string.punctuation).split() 
+                if word not in stopwords.words("english")] for line in raw_corpus] 
 
-    for vec in sentence_list:
+    #count the occurrences of the word in modified data
+    for vec in data_list:
         for word in vec:
             counter[word] += 1
+            
     print most_popular_term(1)
     print least_popular_term(1)
-    #print guitar_types_count(sentence_list)
+    print guitar_types_count(raw_corpus)
     
 def most_popular_term(n):
+    """ Return n most frequent terms from the modified corpus"""
     return counter.most_common(n)
 
 def least_popular_term(n):
+    """ Return n least frequent terms from the modified corpus"""
     return counter.most_common()[-n:]
+
+def guitar_types_count(raw_corpus):
+    """ Return the number of guitar types
+
+    This function collects the list of tokenzed sentences having the word 'guitar' from 'raw_corpus' 
+    to identify guitar types.    
     
+    The problem with this approach is if a word 'guitar' is qualified with an adjective that doesnot 
+    specify type, we generate wrong result
+        for example - decalgirl.com guitar is treated as a type
+    """
+    #tokenize the sentences from the corpus    
+    sentences= nltk.sent_tokenize(raw_corpus.read().lower())  
+    #tokenize words from each sentence in a corpus
+    tokenized_sent=[nltk.word_tokenize(sent) for sent in sentences]
+    #select only those sentences having the owrd 'guitar' in it and do pos tagging
+    sentence_list=[nltk.pos_tag(tsent) for tsent in tokenized_sent if 'guitar' in tsent]
+    #grammar rule for finding guitar type -- adjacective followed by noun
+    rule=r"""ADP: {<JJ><NN>}"""
+    chunker=nltk.RegexpParser(rule)
+    types=set([])
+    for sent in sentence_list:
+        #generate parse tree for each sentence using grammar
+        chunks=chunker.parse(sent)
+        for stree in chunks.subtrees():
+            if stree.node == 'ADP': 
+                # In this program leaves is a list of two tagged words, 
+                #where tagged word is a tuple (word,pos)
+                leaves=stree.leaves()           
+                if 'guitar' in leaves[1]: types.add(leaves[0][0]) 
+    return len(types)
+    
+    
+#explore the file    
 explore_corpus('../data/deals.txt')
