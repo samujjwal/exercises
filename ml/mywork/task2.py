b42b9d036a33d695db7ce980966d6811a4420f9e
samujjwal <samujjwal@outlook.com>
2014-03-30 12:05:03 -0500

    Task2 is updated with modules for generting topic models. Added HDP-LDA model. Need more analysis of topics generated

--- ml/mywork/task2.py
+++ ml/mywork/task2.py
@@ -9,12 +9,14 @@ Building on task 1, we now want to start to understand the relationships to help
 
 """
 import string
+import os
 
 #import logging
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
+import numpy
 
 #logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -43,37 +45,130 @@ def tokenize(doc):
     tokens=[stemmer.stem(word) for word in nltk.word_tokenize(raw) if word not in stopwords.words("english")] 
     return tokens
 
-# Commentout next 5 statements after first run as the results are saved into file for future use
-#docs=process_docs('../data/deals.txt')    
-#dictionary=create_dict(docs)
-#dictionary.save('../models/deals.dict')
-#corpus=create_corpus(docs,dictionary)
-#corpora.MmCorpus.serialize('../models/deals.mm',corpus)
+def create_dict_and_corpus(doc_fname,dict_fname,mm_corpus_fname):
+    """ Returns dictionary and corpus by processing documents
+    
+    dictionary is saved to dict_fname for future access, 
+    corpus is saved in MM format to file corpus_fname
+    
+    """
+    dictionary=None
+    corpus=None
+    if(os.path.isfile(dict_fname)): #check with any file
+        dictionary=corpora.Dictionary.load(dict_fname)
+        corpus=corpora.MmCorpus(mm_corpus_fname)
+    else:
+        docs=process_docs(doc_fname) #preprocess documents by tokenizing
+        dictionary=create_dict(docs)
+        dictionary.save(dict_fname)
+        corpus=create_corpus(docs,dictionary)
+        corpora.MmCorpus.serialize(mm_corpus_fname,corpus)
+    return dictionary,corpus
 
-# Uncomment next 2 statements after first run
-#dictionary=corpora.Dictionary.load('../models/deals.dict')
-#corpus=corpora.MmCorpus('../models/deals.mm')
+def model_with_LSI(corpus,dictionary,num_of_topics,lsi_file):
+    """ Returns LSI topic model trained on corpus and dictionary with for number of topics 
+    given by 'num_of_topics'. If the lsi_file already exist then instead of learning a model
+    the stored model is loaded. 
+    
+    The model is saved to lsi_file when the model is trained.
+    
+    """
+    lsi=None
+    if(os.path.isfile(lsi_file)):
+        #load model instead of training
+        lsi=models.LsiModel.load(lsi_file)
+    else:
+        # Train LSI for topic modeling
+        lsi=models.lsimodel.LsiModel(corpus=corpus, id2word=dictionary, num_topics=num_of_topics)
+        lsi.save(lsi_file)
+    return lsi
+
+
+def model_with_LDA(corpus,dictionary,num_of_topics,lda_file):
+    """ Returns LDA topic model trained on corpus and dictionary with for number of topics 
+    given by 'num_of_topics'. If the lda_file already exist then instead of learning a model
+    the stored model is loaded. 
+    
+    The model is saved to lda_file when the model is trained.
+    
+    """
+    lda=None
+    if(os.path.isfile(lda_file)):
+        #load model instead of training
+        lda=models.LdaModel.load(lda_file)
+    else:
+        # Train LDA model for topic modelling -- using default parameters for now
+        lda=models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_of_topics)
+        lda.save(lda_file)
+    return lda
+
+def model_with_hdplda(corpus,dictionary,hdplda_file):
+    """ Returns LDA topic model trained on corpus and dictionary with from HDP model. 
+    HDP model being non-parametric, the number of topics is inferred. If the hdplda_file already exist then instead of learning a model
+    the stored model is loaded. 
+    
+    The model is saved to hdplda_file when the model is trained.
+    
+    """
+    hdplda=None
+    if(os.path.isfile(hdplda_file)):
+        #load model instead of training
+        hdplda=models.LdaModel.load(hdplda_file)
+    else:
+        # Train HDP model for topic modelling -- using default parameters for now
+        hdp=models.hdpmodel.HdpModel(corpus=corpus, id2word=dictionary)
+        alpha,beta=hdp.hdp_to_lda() #only gives alpha and beta for LDA
+        hdplda=models.LdaModel(id2word=hdp.id2word,num_topics=len(alpha), 
+                               alpha=alpha, eta=hdp.m_eta)
+        hdplda.expElogbeta = numpy.array(beta, dtype=numpy.float32)
+        hdplda.save(hdplda_file)
+    return hdplda
+
+
+def topic_based_clustering(model,dictionary,corpus):
+    return None        
+
+
+
+
+
+
+# Create of Load the dictionary and corpus from the documents file
+doc_fname='../data/deals.txt'
+dict_fname='../models/deals.dict'
+mm_corpus_fname='../models/deals.mm'
+dictionary,corpus=create_dict_and_corpus(doc_fname,dict_fname,mm_corpus_fname)
+
+num_of_topics=100
+
+lsi_file='../models/lsi_100topics.model'
+lsi=model_with_LSI(corpus,dictionary,num_of_topics,lsi_file)
+
+lda_file='../models/lda_100topics.model'
+lda=model_with_LDA(corpus,dictionary,num_of_topics,lsi_file)
+
+hdplda_file='../models/hdplda.model'
+hdplda=model_with_hdplda(corpus,dictionary,hdplda_file)
+
 
 #train TF-IDF model and create 
 #tfidf=models.TfidfModel(corpus)
 #corpus_tfidf=tfidf[corpus]
 
-# Train LDA model for topic modelling 
-#ldamodel=models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=100)
-#ldamodel.save('../models/lda_100topics.model')
 
-ldamodel=models.LdaModel.load('../models/lda_100topics.model')
+#ldamodel=models.LdaModel.load('../models/lda_100topics.model')
 #corpus_ldamodel=ldamodel[corpus_tfidf]
 
-ldatop=open('../results/ldatopics.txt','w')
-for topic in ldamodel.show_topics(10):
-    ldatop.write(str(topic)+"\n")
+#ldatop=open('../results/ldatopics.txt','w')
+#for topic in ldamodel.show_topics(10):
+#    ldatop.write(str(topic)+"\n")
 
-# Train LSI for topic modeling
-#lsi=models.lsimodel.LsiModel(corpus=corpus, id2word=dictionary, num_topics=100)
-#lsi.save('../models/lsi_100topics.model')
+i=0
+aa=hdplda[corpus]
+for a in aa:
+    if i<10:
+        print a
+        i = i+1
+    else:
+        break
 
-lsi=models.LsiModel.load('../models/lsi_100topics.model')
-lsitop=open('../results/lsitopics.txt','w')
-for topic in lsi.show_topics(10):
-    lsitop.write(str(topic)+"\n")
